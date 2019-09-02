5c50133c4e697d3642ec7120c106cc548235f779
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-02 22:24:53 +0800

    Create Virtual_GF.py

---
+++ Virtual_GF.py
@@ -0,0 +1,32 @@
+class GirlFriend :
+    nama = "Mawar"
+    namaku = "Bejo"
+    #constructor
+    def __init__(self,nama,namaku):
+        self.nama = nama
+        self.namaku = namaku
+        print("Your Girl Friend is Ready")
+
+    def sapa(self):
+        print ("Hai sayang" +self.namaku)
+
+    def perhatian(self):
+        print(self.namaku + " sayang sudah makan?, lagi apa?")
+
+    def kenalinNih(self):
+        print("Halo kenalin saya " + self.nama + " pacarnya " + self.namaku)
+
+    def marah(self):
+        print("SAYANG KITA PUTUS!!!")
+
+    def jalan(self):
+        print("Aduh aku sibuk")
+
+    
+
+    
+
+
+
+
+
